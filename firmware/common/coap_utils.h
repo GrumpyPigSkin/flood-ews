33e475dd9ab39c35bacbac66cb3831217a08190b
GrumpyPigSkin <130710602+GrumpyPigSkin@users.noreply.github.com>
2026-06-30 20:48:27 +0100

    Reverted CoAP utils and glaze, if it aint broke don't fix it

---
+++ firmware/common/coap_utils.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024 Alexandre Bailon
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef COAP_UTILS_H
+#define COAP_UTILS_H
+
+#include <openthread/coap.h>
+#include <openthread/ip6.h>
+#include <zephyr/net/openthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define COAP_MAX_BUF_SIZE 128
+#define COAP_DEVICE_ID_SIZE 25
+
+typedef struct {
+  union {
+    otIp6Address addr;
+    char const *str;
+  } u;
+  bool is_str;
+} coap_addr_t;
+
+typedef int (*coap_req_handler_put)(void *ctx, uint8_t *buf, int size);
+typedef int (*coap_req_handler_get)(void *ctx, otMessage *msg,
+                                    const otMessageInfo *msg_info);
+
+int coap_init(void);
+
+int coap_req_handler(void *ctx, otMessage *msg, const otMessageInfo *msg_info,
+                     coap_req_handler_put put_fn, coap_req_handler_get get_fn);
+
+int coap_resp_send(otMessage *req, const otMessageInfo *req_info,
+                   uint8_t const *buf, int len);
+
+int coap_put_req_send(coap_addr_t addr, const char *uri, uint8_t const *buf,
+                      int len, otCoapResponseHandler handler, void *ctx);
+
+int coap_get_req_send(coap_addr_t addr, const char *uri, uint8_t const *buf,
+                      int len, otCoapResponseHandler handler, void *ctx);
+
+const char *coap_device_id(void);
+
+int coap_get_data(otMessage *msg, void *buf, int *len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COAP_UTILS_H */
---
+++ firmware/common/ot_utils.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <openthread/instance.h>
+#include <openthread/link.h>
+
+namespace common {
+
+constexpr std::size_t EUI64_LEN = 8;
+using Eui64Arr = std::array<std::uint8_t, EUI64_LEN>;
+
+/**
+ * @brief Get the eui64
+ * @return otExtAddress
+ */
+inline otExtAddress get_eui64() {
+  otInstance *const ot_inst = openthread_get_default_instance();
+  otExtAddress eui64;
+  otLinkGetFactoryAssignedIeeeEui64(ot_inst, &eui64);
+  return eui64;
+}
+
+/**
+ * @brief Get the eui64 as a uint64_t
+ * @return std::uint64_t
+ */
+inline std::uint64_t get_eui64_as_uint64() {
+  const auto eui = get_eui64();
+  return sys_get_be64(eui.m8);
+}
+
+/**
+ * @brief Get the eui64 as an std::array<std::uint8_t, EUI64_LEN>;
+ * @return std::array<std::uint8_t, EUI64_LEN>;
+ */
+inline Eui64Arr get_eui64_as_arr8() {
+  const auto eui = get_eui64();
+  Eui64Arr eui_out;
+  std::memcpy(eui_out.data(), eui.m8, eui_out.size());
+  return eui_out;
+}
+
+} // namespace common
