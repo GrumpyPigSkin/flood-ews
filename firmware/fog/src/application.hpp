acca6b46e87c2f9e11113be32fd498a5237b41d4
GrumpyPigSkin <130710602+GrumpyPigSkin@users.noreply.github.com>
2026-07-03 12:26:41 +0100

    Added functionality to change collection time when an alert is triggered

--- firmware/fog/src/application.hpp
+++ firmware/fog/src/application.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
+#include "common/alert.hpp"
 #include "common/ot_utils.hpp"
 #include "common/sensor_reading.hpp"
 #include "egress/egress_coordinator.hpp"
 #include "lorawan/service.hpp"
 #include "lorawan/uart_e5.hpp"
+#include "outlier_vote/engine.hpp"
 #include "outlier_vote/sensor_batch.hpp"
 #include "outlier_vote/service.hpp"
 #include "raft/engine.hpp"
 #include "raft/raft_types.hpp"
 #include "sensor/service.hpp"
+#include "utils/time_sync.h"
+#include <cassert>
 
 namespace fog {
 
@@ -33,18 +37,44 @@ public:
 
   void stop() { m_lora_service.stop(); }
 
+  static constexpr std::uint16_t ALERT_INTERVAL_S = 30;
+
 private:
   void on_apply(const raft::Server<>::EntryT &entry) {
+
     if (entry.m_type == raft::EntryType::EGRESS_MSG) {
       egress::EgressRecord egr;
       if (entry.m_data_len == sizeof(egr)) {
         std::memcpy(&egr, &entry.m_data, sizeof(egr));
         m_egress_coordinator.on_egress_applied(egr);
       }
-    }
-    // The egress coordinator will start the message to the gateway.
-    else if (entry.m_type == raft::EntryType::SENSOR_DATA) {
+    } else if (entry.m_type == raft::EntryType::SENSOR_DATA) {
+      // On submit check for an alert and sens immediately.
+      check_alert(entry);
+      // The egress coordinator will start the message to the gateway.
       m_egress_coordinator.begin_egress(entry.m_index);
+    }
+  }
+
+  void check_alert(const raft::Server<>::EntryT &entry) {
+    assert(entry.m_type == raft::EntryType::SENSOR_DATA);
+    batch::SensorBatch batch;
+    std::memcpy(&batch, entry.m_data.data(), sizeof(batch));
+
+    // Send once
+    if (batch.m_alert_active && !m_alert_active) {
+      m_alert_active = true;
+      m_alert_service.send_alert({.m_alert_interval = ALERT_INTERVAL_S,
+                                  .m_alert_active = m_alert_active});
+      m_vote_service.set_collection_window(ALERT_INTERVAL_S);
+      m_vote_service.resync();
+    } else if (!batch.m_alert_active &&
+               m_alert_active != batch.m_alert_active) {
+      m_alert_active = false;
+      m_alert_service.send_alert(
+          {.m_alert_interval = 0, .m_alert_active = m_alert_active});
+      m_vote_service.set_config(vote::Config{});
+      m_vote_service.resync();
     }
   }
 
@@ -67,9 +97,7 @@ private:
                 [this](const batch::SensorBatch &batch, std::uint64_t term,
                        std::uint64_t index, std::uint32_t seq) {
                   (void)m_lora_service.send_batch(batch, term, index, seq);
-                }
-
-    };
+                }};
   }
 
   common::Eui64Arr m_eui = common::get_eui64_as_arr8();
@@ -93,11 +121,18 @@ private:
       },
       common::get_eui64_as_uint64()};
 
-  vote::VoteService m_vote_service{[this](const batch::SensorBatch &batch) {
-    (void)m_raft_engine.submit(
-        raft::EntryType::SENSOR_DATA,
-        std::span(std::bit_cast<const std::byte *>(&batch), sizeof(batch)));
-  }};
+  vote::VoteService m_vote_service{
+      [this](const batch::SensorBatch &batch) {
+        (void)m_raft_engine.submit(
+            raft::EntryType::SENSOR_DATA,
+            std::span(std::bit_cast<const std::byte *>(&batch), sizeof(batch)));
+      },
+      [] -> std::uint64_t {
+        if (const auto time_us = time_sync::get_time_us()) {
+          return time_us.value() / 1000;
+        }
+        return 0;
+      }};
 
   sensor::Service m_sensor_service{
       common::SENSOR_URI, [this](const common::SensorReadingWire &reading) {
@@ -106,6 +141,19 @@ private:
 
   egress::EgressCoordinator<raft::Server<>::EntryT, batch::SensorBatch>
       m_egress_coordinator{make_egress_hooks()};
+
+  common::Alert m_alert_service{[this](const auto alert) {
+    if (alert.m_alert_active) {
+      auto alert_ms = alert.m_alert_interval * 1000;
+      m_vote_service.set_collection_window(alert_ms);
+      m_vote_service.resync();
+    } else {
+      m_vote_service.set_config(vote::Config{});
+      m_vote_service.resync();
+    }
+  }};
+
+  bool m_alert_active;
 };
 
 } // namespace fog
