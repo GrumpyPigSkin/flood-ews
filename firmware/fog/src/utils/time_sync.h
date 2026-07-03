041851cceadf0d60fbc41fd7eea3b1a4e75af580
GrumpyPigSkin <130710602+GrumpyPigSkin@users.noreply.github.com>
2026-07-03 12:22:28 +0100

    Added time sync feature

---
+++ firmware/fog/src/utils/time_sync.h
@@ -0,0 +1,26 @@
+
+
+#include "openthread.h"
+#include <cstdint>
+#include <openthread/network_time.h>
+#include <optional>
+
+namespace fog::time_sync {
+
+/**
+ * @brief Get synchronised network time in microseconds.
+ * @return std::optional<std::uint64_t> std::nullopt if not synced.
+ */
+inline std::optional<std::uint64_t> get_time_us() {
+  otInstance *const ot_inst = openthread_get_default_instance();
+  std::uint64_t network_time_us = 0;
+  otNetworkTimeStatus status = otNetworkTimeGet(ot_inst, &network_time_us);
+
+  if (status != OT_NETWORK_TIME_SYNCHRONIZED) {
+    return std::nullopt;
+  }
+
+  return network_time_us;
+}
+
+} // namespace fog::time_sync
