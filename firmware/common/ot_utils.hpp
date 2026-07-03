cf871726f88abe12b2d398c19862ec03e2e42ab0
GrumpyPigSkin <130710602+GrumpyPigSkin@users.noreply.github.com>
2026-07-03 10:43:09 +0100

    Added multicast message for all endpoints

--- firmware/common/ot_utils.hpp
+++ firmware/common/ot_utils.hpp
@@ -12,6 +12,7 @@
 namespace common {
 
 constexpr auto *MESH_LOCAL_MULTICAST_ADDR = "ff03::2";
+constexpr auto *MESH_LOCAL_FTD_MTD_MULTICAST_ADDR = "ff03::1";
 
 constexpr std::size_t EUI64_LEN = 8;
 using Eui64Arr = std::array<std::uint8_t, EUI64_LEN>;
