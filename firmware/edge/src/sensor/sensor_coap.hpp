6afb1e1668a49681777ed61190f4de9443b001e8
GrumpyPigSkin <130710602+GrumpyPigSkin@users.noreply.github.com>
2026-07-29 22:04:31 +0100

    Added fault injection, protected behind a #define ENABLE_FAULT_INJECTION

--- firmware/edge/src/sensor/sensor_coap.hpp
+++ firmware/edge/src/sensor/sensor_coap.hpp
@@ -4,9 +4,12 @@
 #include <common/coap_utils.h>
 #include <common/ot_utils.hpp>
 #include <common/sensor_reading.hpp>
-#include <cstddef>
 #include <cstdint>
 #include <mutex>
+
+#ifdef ENABLE_FAULT_INJECTION
+#include "fault_injection/fault_injection.hpp"
+#endif
 
 namespace edge::sensor {
 
@@ -24,6 +27,12 @@ public:
       : m_eui(common::get_eui64_as_uint64()), m_address(address),
         m_uri_path{uri_path} {}
 
+  void init() {
+#ifdef ENABLE_FAULT_INJECTION
+    m_fault_injection.init();
+#endif
+  }
+
   /**
    * @brief Send the sensor reading over Thread through a CoAP put message.
    * @param [in] reading The sensor reading.
@@ -36,6 +45,16 @@ public:
                                       .m_validity = reading.m_validity,
                                       .m_detail = reading.m_detail,
                                       .m_seq = m_seq_id};
+
+#ifdef ENABLE_FAULT_INJECTION
+    const auto fault_message = m_fault_injection.get_fault();
+    if (fault_message.m_active) {
+      to_wire.m_water_level_mm = fault_message.m_bad_reading.m_water_level;
+      to_wire.m_validity = fault_message.m_bad_reading.m_validity;
+      to_wire.m_detail = fault_message.m_bad_reading.m_detail;
+      to_wire.m_seq = fault_message.m_bad_reading.m_seq;
+    }
+#endif
 
     // Increment the sequence id.
     m_seq_id++;
@@ -62,6 +81,10 @@ private:
 
   /** @brief The uri to send to. */
   const char *m_uri_path{nullptr};
+
+#ifdef ENABLE_FAULT_INJECTION
+  fault::FaultInjection m_fault_injection;
+#endif
 
   /** @brief Lock for openthread API access. */
   mutable common::openthread_mutex m_otmx;
